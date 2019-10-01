695046a985b114f9f6a589aec257f5e40578da19
Ubuntu <ubuntu@ip-172-31-20-77.ec2.internal>
2019-10-01 21:00:22 +0000

    commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if InventoryItem.all.present?
+    
+    InventoryItem.all.each do |i|
+        i.destroy
+    end
+    
+else
+    
+    500.times.each do
+        InventoryItem.create(
+            item_name: Faker::Lorem.sentence(word_count: 5),
+            item_brand: Faker::Lorem.word,
+            item_description: Faker::Lorem.paragraph(sentence_count: 15),
+            item_condition: ["Excellent", "Good", "Fair"][rand(0..2)],
+            item_value: ["100", "149.99", "199.99", "239.99", "399.99"][rand(0..5)],
+            in_stock: true,
+            item_owner_id: [6, 4, 3][rand(0..2)]
+            )
+    end
+    
+end
