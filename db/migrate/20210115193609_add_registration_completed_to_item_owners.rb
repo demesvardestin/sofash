ef071459c9f87ce401fc37981aac5e70ca465a56
Ubuntu <ubuntu@ip-172-31-34-183.us-east-2.compute.internal>
2021-03-06 15:40:03 +0000

    commit

--- db/migrate/20210115193609_add_registration_completed_to_item_owners.rb
+++ db/migrate/20210115193609_add_registration_completed_to_item_owners.rb
@@ -2,4 +2,11 @@ class AddRegistrationCompletedToItemOwners < ActiveRecord::Migration[5.0]
   def change
     add_column :item_owners, :registration_completed, :boolean, default: false
   end
+  
+  def change
+    create_table :orders do |t|
+
+      t.timestamps
+    end
+  end
 end
--- db/migrate/20210128183708_add_details_to_orders.rb
+++ db/migrate/20210128183708_add_details_to_orders.rb
@@ -1,14 +1,5 @@
 class AddDetailsToOrders < ActiveRecord::Migration[5.0]
   def change
-    create_table :orders do |t|
-
-      t.timestamps
-    end
-    
-    add_column :images, :first_source, :string
-  end
-  
-  def change
     add_column :orders, :inventory_item_id, :integer
     add_column :orders, :item_owner_id, :integer
     add_column :orders, :renter_id, :integer
