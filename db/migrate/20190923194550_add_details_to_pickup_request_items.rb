a3e108742ce3f341f374b1f1b2ef09fa666c2459
Ubuntu <ubuntu@ip-172-31-34-183.us-east-2.compute.internal>
2021-03-06 15:22:58 +0000

    commit

--- db/migrate/20190923194550_add_details_to_pickup_request_items.rb
+++ db/migrate/20190923194550_add_details_to_pickup_request_items.rb
@@ -1,6 +1,6 @@
 class AddDetailsToPickupRequestItems < ActiveRecord::Migration[5.0]
   def change
-    add_column :pickup_request_items, :pickup_request_id, :integer
-    add_column :pickup_requests, :item_owner_id, :integer
+    # add_column :pickup_request_items, :pickup_request_id, :integer
+    # add_column :pickup_requests, :item_owner_id, :integer
   end
 end
--- db/migrate/20190924202841_add_submitted_to_pickup_requests.rb
+++ db/migrate/20190924202841_add_submitted_to_pickup_requests.rb
@@ -1,7 +1,7 @@
 class AddSubmittedToPickupRequests < ActiveRecord::Migration[5.0]
   def change
-    add_column :pickup_requests, :submitted, :boolean, default: false
-    add_column :pickup_requests, :confirmation_token, :string
+    # add_column :pickup_requests, :submitted, :boolean, default: false
+    # add_column :pickup_requests, :confirmation_token, :string
     add_column :inventory_items, :item_owner_id, :integer
   end
 end
