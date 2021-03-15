7a8fb3ac7436084d06fb552ac270b7509f27ed73
Ubuntu <ubuntu@ip-172-31-34-183.us-east-2.compute.internal>
2021-03-15 00:05:07 +0000

    commit

--- app/controllers/stripe_webhooks_controller.rb
+++ app/controllers/stripe_webhooks_controller.rb
@@ -1,4 +1,6 @@
 class StripeWebhooksController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    
     def account
         render status: 200, :nothing => true
     end
